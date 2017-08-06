9b7f5dd0a1895699d6c021763fcc6fac5285f21e
Syed Aman <sna274@nyu.edu>
2017-08-06 18:31:54 -0500

    create a fake GraphQL database and root value

--- src/index.js
+++ src/index.js
@@ -9,9 +9,36 @@ let app = express()
 // use node environment specified port or default to 8050
 const PORT = process.env.port || 8050
 
+// create a fake database according to defined schemas to test GraphQL
+const fakeDatabase = {
+  'mohammed': {
+    id: 1,
+    firstName: 'Mohammed',
+    lastName: 'Yaseen'
+  },
+  'tomnoy': {
+    id: 2,
+    firstName: 'Lizaful',
+    lastName: 'Tomnoy'
+  },
+  'syed': {
+    id: 3,
+    firstName: 'Syed',
+    lastName: 'Aman'
+  }
+}
+
+// define GraphQL root value
+const rootValue = {
+  user: function ({id}) {
+    return fakeDatabase[id]
+  }
+}
+
 // mount GraphQL to http://localhost:8050/graphql
 app.use('/graphql', expressGraphQL({
   schema: schema,
+  rootValue: rootValue,
   graphiql: true,
   pretty: true
 }))
@@ -21,5 +48,5 @@ app.listen(PORT, (error) => {
   // throw an error if present
   if (error) throw error
 
-  console.log(`http server running on http://localhost:${PORT}`)
+  console.log(`Running a GraphQL API server on http://localhost:${PORT}`)
 })
