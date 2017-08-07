57efbcef32ae6328d1bd4b3c12bbae3499d279ef
Syed Aman <sna274@nyu.edu>
2017-08-07 11:55:12 -0500

    handle database connection successes or errors

--- src/index.js
+++ src/index.js
@@ -8,12 +8,23 @@ import fakeDatabase from './fakeDatabase'
 
 let app = express()
 
+const mongoDbRoute = 'mongodb://localhost:27017/local'
+
 // connect to local MongoDB database using mongoose
 // @NOTE make sure to install MongoDB
-const databasePromise = mongoose.connect('mongodb://localhost:27017/local', {
+const databasePromise = mongoose.connect(mongoDbRoute, {
   useMongoClient: true
 })
 
+// handle database connection errors
+databasePromise.on('error', () => {
+  console.log(`Failed to connect mongoose to ${mongoDbRoute}`)
+})
+
+// handle successful database connection
+databasePromise.once('open', () => {
+  console.log(`Successfully connected mongoose to ${mongoDbRoute}`)
+})
 
 // use node environment specified port or default to 8050
 const PORT = process.env.port || 8050
