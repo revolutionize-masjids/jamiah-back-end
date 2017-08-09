e0f8d5477d5b4eba62be3fbc0d52d3c624534f02
Syed Aman <sna274@nyu.edu>
2017-08-09 11:16:47 -0500

    define schema for Moderator. Improve schema for Users. Create an interface to represent people.

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,12 +2,31 @@
 
 import { buildSchema } from 'graphql'
 
+/** GraphQL schema expressed in shorthand notation */
 const schema = buildSchema(`
-  # User schema
-  type User {
+  interface Person {
     id: ID!
     firstName: String
     lastName: String
+    is_online: Boolean
+  }
+
+  # Schema for all users
+  type User implements Person {
+    id: ID!
+    firstName: String
+    lastName: String
+    is_online: Boolean
+    reputation: Int
+  }
+
+  # Schema for moderators
+  type Moderator implements Person {
+    id: ID!
+    firstName: String
+    lastName: String
+    is_online: Boolean
+    usersBanned: [User]!
   }
 
   # Root Query
