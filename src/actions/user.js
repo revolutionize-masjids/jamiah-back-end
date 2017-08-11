a5291ea24ab61b29c404b2ff27fcf248ced089c4
Syed Aman <sna274@nyu.edu>
2017-08-11 16:27:45 -0500

    validate the POST /users

--- src/actions/user.js
+++ src/actions/user.js
@@ -5,12 +5,16 @@
 // use a router to make use of the express server instance in different files outside of // index.js for "seperation of concerns"
 import express from 'express'
 const Router = express.Router()
+import assert from 'assert'
 
 // import models
 import User from '../mongoose/models/user'
 
 // define a POST route to create users
 Router.post('/users', (request, response) => {
+  // validate request
+  assert(request.body.firstName && request.body.lastName, 'invalid request body')
+
   // create a user using the user model
   const newUser = new User({
     firstName: request.body.firstName,
