16b9e71569fe814fb7b8c7febcf6ca8ed699cc0b
Syed Aman <sna274@nyu.edu>
2017-08-11 18:29:22 -0500

    print errors to the s erver console that are usually swallowed by GraphQL

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -8,7 +8,11 @@ const executableSchema = makeExecutableSchema({
   // all created schemas
   typeDefs: rootSchema,
   // all created resolvers
-  resolvers: rootResolvers
+  resolvers: rootResolvers,
+  // print errors to the server console that are usually swallowed by GraphQL
+  logger: {
+    log: (error) => console.log(error)
+  }
 })
 
 // expose the GraphQLSchemaInstance to be used by the server
