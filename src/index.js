0ec6a072a77393b6789473cf9cc788b0e2b96c63
Syed Aman <sna274@nyu.edu>
2017-08-11 16:36:03 -0500

    add some comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@
 // entry point for the server
 //
 
-// use express to create the http server
 import express from 'express'
-// use body-parser to access HTTP request bodies and the data they contain
 import bodyParser from 'body-parser'
-// use express-graphql to interface with graphql
 import expressGraphQL from 'express-graphql'
 
 // run mongoose scripts to manage MongoDB database
@@ -18,29 +15,30 @@ import schema from './graphql'
 // get the defined API routes
 import apiRoutes from './actions/user'
 
-// create a variable to represent "the server"
+// create an object that represents "the server"
 let app = express()
 
 // use node environment specified port or default to 8050
 const PORT = process.env.port || 8050
 
-// make the api ONLY accept HTTP requests in the form of JSON
-// parse the request's data chunks into a use-able body
+// make the api ONLY accept HTTP requests in the form of JSON. use body-parser
+// to access request bodies by parsing its data chunks
 app.use(bodyParser.json({ type: 'application/json' }))
 
 // use the defined API routes
 app.use(apiRoutes)
 
-// mount GraphQL to http://localhost:8050/graphql
+// use express-graphql to interface with GraphQL and mount GraphQL to
+// http://localhost:8050/graphql
 app.use('/graphql', expressGraphQL({
   schema: schema,
   graphiql: true,
   pretty: true
 }))
 
-// run the http server on port 8050
-app.listen(PORT, (error) => {
-  // throw an error if present
+// run a graphql api http server on port 8050
+let server = app.listen(PORT, (error) => {
+  // handle errors
   if (error) throw error
 
   console.log(`Running a GraphQL API server on http://localhost:${PORT}`)
