d68ef1b9f9e1819fd8f64e855fddb78a26089eb8
Syed Aman <sna274@nyu.edu>
2017-08-13 11:03:43 -0500

    change port 8050 to port 8091

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ import apiRoutes from './actions/user'
 // create an object that represents "the server"
 let app = express()
 
-// use node environment specified port or default to 8050
-const PORT = process.env.port || 8050
+// use node environment specified port or default to 8091
+const PORT = process.env.port || 8091
 
 // make the api ONLY accept HTTP requests in the form of JSON. use body-parser
 // to access request bodies by parsing its data chunks
@@ -29,14 +29,14 @@ app.use(bodyParser.json({ type: 'application/json' }))
 app.use(apiRoutes)
 
 // use express-graphql to interface with GraphQL and mount GraphQL to
-// http://localhost:8050/graphql
+// http://localhost:8091/graphql
 app.use('/graphql', expressGraphQL({
   schema: schema,
   graphiql: true,
   pretty: true
 }))
 
-// run a graphql api http server on port 8050
+// run a graphql api http server on port 8091
 let server = app.listen(PORT, (error) => {
   // handle errors
   if (error) throw error
