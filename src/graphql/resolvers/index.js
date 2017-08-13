1c9be9635c3948a7775d193cbe38fe32b310b2fa
Syed Aman <sna274@nyu.edu>
2017-08-13 15:44:09 -0500

    improve async await syntax and success/error handling

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,20 +1,34 @@
+import User from '../../mongoose/models/user'
+
 // define the resolvers for every query
 const rootResolvers = {
-  RootQuery:{
+  RootQuery: {
     allusers: async (parent, args,{ User }) => {
       // {id: 11221, name:"blah blah"}
-      const users = await User.find();
+      const users = await User.find()
       return users.map((x) => {
-        x.id = x.id.toString();
+        x.id = x.id.toString()
         return x;
       })
     },
-  };
-  RootMutation:{
-    createUser: async (parent, args,{ User }) => {
-      const user = await new User(args).save();
-        user.id = user.id.toString();
-        return user;
+  }
+
+  RootMutation: {
+    // create a user using the User model
+    createUser: async (parent, {firstName, lastName}, context) => {
+      try {
+        // save the new user to the collection
+        const newUser = await new User(args).save()
+        // stringify the id for MongoDB purposes
+        newUser.id = newUser.id.toString()
+
+        console.log(`successfully saved user ${newUser.firstName} to the database collection`)
+
+        return newUser
+      } catch (error) {
+        // handle errors
+        console.log(`failed to save user ${newUser.firstName}`, error)
+      }
     },
   },
 };
