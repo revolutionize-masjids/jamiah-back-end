59a8db7c5dc3b026a0a0a883c8ff194251555d13
Your Name <myy233@nyu.edu>
2017-08-13 16:01:42 -0400

    setup resolver function for users

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,2 +1,20 @@
 // define the resolvers for every query
-const rootResolvers = {}
+const rootResolvers = {
+  RootQuery:{
+    allusers: async (parent, args,{ User }) => {
+      // {id: 11221, name:"blah blah"}
+      const users = await User.find();
+      return users.map((x) => {
+        x.id = x.id.toString();
+        return x;
+      })
+    },
+  };
+  Mutation:{
+    createUser: async (parent, args,{ User }) => {
+      const user = await new User(args).save();
+        user.id = user.id.toString();
+        return user;
+      })
+    },
+  };  
