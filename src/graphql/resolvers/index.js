c092fd9bc6255dd1b01694a1b913e8e27a3be93e
Your Name <myy233@nyu.edu>
2017-08-13 16:29:40 -0400

    correct import

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,20 +1,19 @@
 // define the resolvers for every query
-import rootMutations from './mutations'
-import rootQueries from './queries'
+import User from '../mongoose/models/user'
 
 const rootResolvers = {
-  RootQuery:{
-    allusers: async (parent, args,{ User }) => {
+  RootQuery: {
+    allUsers: async (parent, args, { User }) => {
       // {id: 11221, name:"blah blah"}
       const users = await User.find();
       return users.map((x) => {
         x.id = x.id.toString();
         return x;
-      })
+      });
     },
   };
-  RootMutation:{
-    createUser: async (parent, args,{ User }) => {
+  RootMutation: {
+    createUser: async (parent, args, { User }) => {
       const user = await new User(args).save();
         user.id = user.id.toString();
         return user;
