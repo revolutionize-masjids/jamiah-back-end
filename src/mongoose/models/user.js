cde567b59325bea6a33d105d42363b1f605611f7
Syed Aman <sna274@nyu.edu>
2017-08-13 18:44:47 -0500

    update user schema

--- src/mongoose/models/user.js
+++ src/mongoose/models/user.js
@@ -7,9 +7,10 @@ import autoIncrement from 'mongoose-auto-increment'
 
 // describe the properties that will be in every user record
 const userSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId },
   firstName: String,
-  lastName: String
+  lastName: String,
+  is_online: Boolean,
+  reputation: Number
 }, { collection: "user" }) // use this schema in the collection named "user"
 
 // TODO: find a better way to add in a plugin since this is deprecated.
