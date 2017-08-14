53b6be91655e7fba1f333e702faacec13e78e28f
Syed Aman <sna274@nyu.edu>
2017-08-13 19:55:16 -0500

    create a file specifically dedicated to resolvers for queries

---
+++ src/graphql/resolvers/queries.js
@@ -0,0 +1,37 @@
+//
+// define the resolvers for every query
+//
+
+const queryResolvers = {
+  // resolvers for all queries
+  RootQuery: {
+    // get a specific user
+    user: async (parent, args, context) => {
+      // search the database for one user that fulfills arguments
+      const user = await User.findOne(args)
+
+      return user
+    },
+
+    // get all users that match filter parameters
+    users: async (parent, args, context) => {
+      // do a database query to search for all users that match those filters
+      const users = await User.find(args)
+
+      return users
+    },
+
+    // get all created users
+    allUsers: async (parent, args, context) => {
+      // do a database query to search the collection for all Users
+      const users = await User.find()
+
+      // stringify ids for MongoDB purposes
+      return users.map((x) => {
+        x.id = x.id.toString()
+        return x
+      })
+    }
+}
+
+export default queryResolvers
