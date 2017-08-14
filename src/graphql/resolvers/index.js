7f0b537c19f05d7cb7fbf26539357fb94e3a3d31
Syed Aman <sna274@nyu.edu>
2017-08-13 19:45:12 -0500

    add query to get all users that match filter parameters

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -7,6 +7,22 @@ import User from '../../mongoose/models/user'
 const rootResolvers = {
   // resolvers for all queries
   RootQuery: {
+    // get a specific user
+    user: async (parent, args, context) => {
+      // search the database for one user that fulfills arguments
+      const user = await User.findOne(args)
+
+      return user
+    },
+
+    // get all users that match filter parameters
+    users: async (parent, args, context) => {
+      // do a database query to search for all users that match those filters
+      const users = await User.find(args)
+
+      return users
+    },
+
     // get all created users
     allUsers: async (parent, args, context) => {
       // do a database query to search the collection for all Users
@@ -17,14 +33,6 @@ const rootResolvers = {
         x.id = x.id.toString()
         return x
       })
-    },
-
-    // get a specific user
-    user: async (parent, args, context) => {
-      // search the database for one user that fulfills arguments
-      const user = await User.findOne(args)
-
-      return user
     }
   },
 
--- src/graphql/typeDefs/queries.js
+++ src/graphql/typeDefs/queries.js
@@ -2,14 +2,14 @@
 const rootQueries = [`
   # Root Query: Allow clients to read records
   type RootQuery {
-    # get current user
-    me: User
-
-    # get all users
-    allUsers: [User]!
-
     # get a user
     user(firstName: String, lastName: String, id: ID): User
+
+    # get all users that match filter parameters
+    users(firstName: String, lastName: String, is_online: Boolean): [User]
+
+    # get every existing user
+    allUsers: [User]!
   }
 `]
 
