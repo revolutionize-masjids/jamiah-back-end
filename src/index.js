aca1db1d1f364a7f667034ec543cb9a553b1294e
Syed Aman <sna274@nyu.edu>
2017-08-19 11:24:39 -0500

    use bodyParser only for POSTs

--- src/index.js
+++ src/index.js
@@ -19,13 +19,12 @@ let app = express()
 // use node environment specified port or default to 8091
 const PORT = process.env.port || 8091
 
+// use apollo-server-express to interface with GraphQL and mount GraphQL to
+// CORS-enabled http://localhost:8091/graphql
 // make the api ONLY accept HTTP requests in the form of JSON. use body-parser
 // to access request bodies by parsing its data chunks
-app.use(bodyParser.json({ type: 'application/json' }))
-
-// use express-graphql to interface with GraphQL and mount GraphQL to
-// CORS-enabled http://localhost:8091/graphql
-app.use('/graphql', cors(), graphqlExpress({
+// bodyParser is only needed for POSTs
+app.use('/graphql', bodyParser.json(), cors(), graphqlExpress({
   schema: executableSchema,
   graphiql: true,
   pretty: true
