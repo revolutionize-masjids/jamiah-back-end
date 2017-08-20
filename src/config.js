d51c992726350d34d085c53c3a6cb1b2fc63c064
Syed Aman <sna274@nyu.edu>
2017-08-20 11:23:34 -0500

    add useful comments

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
-// var config = require('./config.js').get(process.env.NODE_ENV);
+// define secret configuration for development vs. production
 
 const config = {
   production: {
+    // remote mlabs database
     database: 'mongodb://general-user:user123@ds145010.mlab.com:45010/eejmc',
   },
   development: {
+    // NOTE: make sure to have MongoDB running locally
     database: 'mongodb://127.0.0.1:27017/EEJMC',
   }
 }
