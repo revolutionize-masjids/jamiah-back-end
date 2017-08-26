72cf89539ad29f6da9c694d175fc1194389bf5a2
Syed Aman <sna274@nyu.edu>
2017-08-26 15:18:45 -0500

    define the event model schema

---
+++ src/mongoose/models/event.js
@@ -0,0 +1,53 @@
+//
+// Event model definition
+//
+
+import mongoose, { Schema } from 'mongoose'
+
+import { UserSchema } from './user'
+
+// describe the properties that will be in every event record
+const EventSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required:  true
+  },
+  dateStart: String,
+  dateEnd: String,
+  address: {
+    type: String,
+    required: true
+  },
+  attendees: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  likes: {
+    type: [UserSchema],
+    required: false,
+    default: []
+  },
+  subscribedUsers: {
+    type: [UserSchema],
+    required: false,
+    default: []
+  },
+  volunteers: {
+    type: [UserSchema],
+    required: false,
+    default: []
+  }
+}, { collection: "event" }) // use this schema in the collection named "event"
+
+// using the event schema, create a model for events
+const Event = mongoose.model('Event', EventSchema)
+
+export {
+  Event as default,
+  EventSchema
+}
